b22df5ea6d884634d2fe470216c4f64bba06c10c
vitorjusto <vitorhugojusto1@gmail.com>
2025-12-17 10:00:27 -0300

    add button to door

--- Source/Door.cs
+++ Source/Door.cs
@@ -1,6 +1,6 @@
+using System;
 using Godot;
 using IceGame.Source;
-using System;
 
 public partial class Door : Node2D
 {
@@ -9,22 +9,42 @@ public partial class Door : Node2D
     public int Id;
     [Export]
     public string Path;
+    private OpacityChangerNode _opacityChanger;
 
-    // Called when the node enters the scene tree for the first time.
     public override void _Ready()
-	{
-	}
+        => _opacityChanger = new OpacityChangerNode(GetNode<Node2D>("PlayerButtonUp"));
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
         if(_isDlayerFrontDoor && Input.IsActionJustPressed("Up"))
             LevelManager.GetManager().StartLevelTransition(Id, Path);
 
+		_opacityChanger.Active = _isDlayerFrontDoor;
+		_opacityChanger.Process(delta);
     }
 
     public void OnPlayerEntered(Node2D node)
         => _isDlayerFrontDoor = true;
     public void OnPlayerExited(Node2D node)
         => _isDlayerFrontDoor = false;
+}
+public class OpacityChangerNode
+{
+    public bool Active;
+    private readonly Node2D _node;
+    private double _opacity;
+    public OpacityChangerNode(Node2D node)
+    {
+        _node = node;
+    }
+    public void Process(double delta)
+    {
+        if(Active)
+            _opacity += 600 * delta;
+        else
+            _opacity -= 600 * delta;
+			
+        _opacity = Math.Clamp(_opacity, 0, 255);
+        _node.Modulate = Color.Color8(255, 255, 255, (byte)_opacity);
+    }
 }
